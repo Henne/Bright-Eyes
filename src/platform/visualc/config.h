5623a772b2431dc33f133cb2dc886fa2c854c35e
Henrik Kretzschmar <henne@nachtwindheim.de>
2010-11-22 11:45:32 +0100

    Windows: disable deps on SDL_net and libpng
    This diaables dependencies on SDL_net and libpng, which means you'll
    currently
    * can't make screenshots (will be enabled again soon) and,
    * have no network support in DOSBox (not needed for DSA games)

--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -4,16 +4,16 @@
 #define C_DEBUG 0
 
 /* Define to 1 to enable screenshots, requires libpng */
-#define C_SSHOT 1
+#define C_SSHOT 0
 
 /* Define to 1 to use opengl display output support */
 #define C_OPENGL 1
 
 /* Define to 1 to enable internal modem support, requires SDL_net */
-#define C_MODEM 1
+#define C_MODEM 0
 
 /* Define to 1 to enable IPX networking support, requires SDL_net */
-#define C_IPX 1
+#define C_IPX 0
 
 /* Enable some heavy debugging options */
 #define C_HEAVY_DEBUG 0
