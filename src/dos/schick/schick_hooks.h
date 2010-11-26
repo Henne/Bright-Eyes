73d8d62999552bce81969cdffbfde06d1e29961b
Henrik Kretzschmar <henne@nachtwindheim.de>
2010-11-26 16:13:09 +0100

    add some docu to hooks header

--- src/dos/schick/schick_hooks.h
+++ src/dos/schick/schick_hooks.h
@@ -1,5 +1,8 @@
+/* prototypes for Execution operations */
 void init_schick(char *, unsigned short, unsigned short, unsigned short);
 void exit_schick(unsigned char);
+
+/* prototypes for FILE operations */
 
 void schick_create(const char *, unsigned char, unsigned);
 void schick_open(const char *, unsigned char, unsigned);
@@ -9,6 +12,9 @@ void schick_read(unsigned, unsigned char*, unsigned short);
 void schick_write(unsigned, unsigned char*, unsigned short);
 
 void schick_seek(unsigned, unsigned, unsigned);
+
+
+/* prototypes for CPU operations */
 int schick_callf(unsigned, unsigned);
-void schick_ret();
 int schick_calln16(unsigned);
+void schick_ret();
