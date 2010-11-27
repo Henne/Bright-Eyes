86ccb5f709fb57a07eb94a5ee6addcc092dbd772
Henrik Kretzschmar <henne@nachtwindheim.de>
2010-11-27 15:20:06 +0100

    Make a configure option to disable brighteyes
    With the following command, you can disable brighteyes.
    
    So, you can build the original DOSBox, without any overhead.
    
    ./configure --disable-schick

--- src/dos/schick/schick_hooks.h
+++ src/dos/schick/schick_hooks.h
@@ -1,3 +1,5 @@
+#ifdef DOSBOX_SCHICK
+
 /* prototypes for Execution operations */
 void init_schick(char *, unsigned short, unsigned short, unsigned short);
 void exit_schick(unsigned char);
@@ -18,3 +20,28 @@ void schick_seek(unsigned, unsigned, unsigned);
 int schick_callf(unsigned, unsigned);
 int schick_calln16(unsigned);
 void schick_ret();
+
+#else
+
+/* prototypes for Execution operations */
+inline void init_schick(char *, unsigned short, unsigned short, unsigned short) {};
+inline void exit_schick(unsigned char) {};
+
+/* prototypes for FILE operations */
+
+inline void schick_create(const char *, unsigned char, unsigned) {};
+inline void schick_open(const char *, unsigned char, unsigned) {};
+inline void schick_close(unsigned) {};
+
+inline void schick_read(unsigned, unsigned char*, unsigned short) {};
+inline void schick_write(unsigned, unsigned char*, unsigned short) {};
+
+inline void schick_seek(unsigned, unsigned, unsigned) {};
+
+
+/* prototypes for CPU operations */
+inline int schick_callf(unsigned, unsigned) {return 0;};
+inline int schick_calln16(unsigned) {return 0;};
+inline void schick_ret() {};
+
+#endif
