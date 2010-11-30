8f33ca6c39d5364a3f40b0461bbbb5caa9477474
Henrik Kretzschmar <henne@nachtwindheim.de>
2010-11-30 23:36:16 +0100

    avoid double definitions

--- include/custom_hooks.h
+++ include/custom_hooks.h
@@ -1,3 +1,6 @@
+#ifndef DOSBOX_SCHICK_H
+#define DOSBOX_SCHICK_H
+
 #ifdef DOSBOX_SCHICK
 
 /* prototypes for Execution operations */
@@ -21,7 +24,7 @@ int schick_callf(unsigned, unsigned);
 int schick_calln16(unsigned);
 void schick_ret();
 
-#else
+#else /* DOSBOX_SCHICK */
 
 /* prototypes for Execution operations */
 inline void init_schick(char *, unsigned short, unsigned short, unsigned short) {};
@@ -44,4 +47,6 @@ inline int schick_callf(unsigned, unsigned) {return 0;};
 inline int schick_calln16(unsigned) {return 0;};
 inline void schick_ret() {};
 
-#endif
+#endif /* DOSBOX_SCHICK */
+
+#endif /* DOSBOX_SCHICK_H */
