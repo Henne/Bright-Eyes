d43d8ac3738178c177b2b1c4c1140d1a66386620
Henrik Kretzschmar <henne@nachtwindheim.de>
2011-01-12 09:18:54 +0100

    add seg105 files to the project

---
+++ src/custom/schick_rewrite/seg105.cpp
@@ -0,0 +1,10 @@
+/*
+ *      Rewrite of DSA1 v3.02_de functions of seg105 (inventory)
+ *      Functions rewritten 0/14
+ *
+ *      Functions called rewritten 0/13
+ *      Functions uncalled rewritten 0/1
+*/
+
+#include "schick.h"
+
