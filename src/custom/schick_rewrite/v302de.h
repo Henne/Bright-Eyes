f8b47cf41c9004d2923790d1bf373b0234e28929
Henrik Kretzschmar <henne@nachtwindheim.de>
2011-01-13 11:59:36 +0100

    add get_itemsdat()
    This helper function can be used to access the content of ITEMS.DAT

--- src/custom/schick_rewrite/v302de.h
+++ src/custom/schick_rewrite/v302de.h
@@ -9,7 +9,12 @@
 #include "schick.h"
 
 #define TEXT_LTX	(0xc3b5)
+#define ITEMSDAT	(0xe22b)
 
 static inline Bit8u *get_ltx(unsigned short off) {
 	return MemBase + Real2Phys(mem_readd(Real2Phys(ds_readd(TEXT_LTX) + off)));
+}
+
+static inline Bit8u *get_itemsdat(unsigned short item) {
+	return MemBase + Real2Phys(ds_readd(ITEMSDAT)) + item * 12;
 }
