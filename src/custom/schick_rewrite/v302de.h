0ecef4d2f6aa93769b53a1bc85a2d271256506d2
Henrik Kretzschmar <henne@nachtwindheim.de>
2011-01-20 12:44:11 +0100

    add 2 chessboard acessing functions

--- src/custom/schick_rewrite/v302de.h
+++ src/custom/schick_rewrite/v302de.h
@@ -10,6 +10,7 @@
 
 #define HEROS		(0xbd34)
 #define TEXT_LTX	(0xc3b5)
+#define CHESSBOARD	(0xd852)
 #define ITEMSDAT	(0xe22b)
 
 static inline Bit8u *get_hero(unsigned short index) {
@@ -18,6 +19,14 @@ static inline Bit8u *get_hero(unsigned short index) {
 
 static inline Bit8u *get_ltx(unsigned short off) {
 	return MemBase + Real2Phys(mem_readd(Real2Phys(ds_readd(TEXT_LTX) + off)));
+}
+
+static inline signed char get_cb_val(unsigned short x, unsigned short y) {
+	return host_readb(MemBase + Real2Phys(ds_readd(CHESSBOARD)) + y * 25 + x);
+}
+
+static inline void set_cb_val(unsigned short x, unsigned short y, signed char val) {
+	host_writeb(MemBase + Real2Phys(ds_readd(CHESSBOARD)) + y * 25 + x, val);
 }
 
 static inline Bit8u *get_itemsdat(unsigned short item) {
