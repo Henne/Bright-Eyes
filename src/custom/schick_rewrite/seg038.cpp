e5bee5176090fbe5228b77b65b1ba5def9b0afc8
Henrik Kretzschmar <henne@nachtwindheim.de>
2011-01-21 18:01:55 +0100

    add seg038 (Fight) to the project

---
+++ src/custom/schick_rewrite/seg038.cpp
@@ -0,0 +1,7 @@
+/*
+ *      Rewrite of DSA1 v3.02_de functions of seg038 (Fight)
+ *      Functions rewritten 0/6
+ *
+ *      Functions called rewritten 0/5
+ *      Functions uncalled rewritten 0/1
+*/
