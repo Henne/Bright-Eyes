9d393735a69e3d449277c6dcdfdee92641b38b4b
Hendrik Radke <hermes9@web.de>
2011-10-23 20:09:53 +0200

    Added support for DSA2 (Sternenschweif).

--- src/custom/custom.cpp
+++ src/custom/custom.cpp
@@ -8,10 +8,6 @@
 #include "custom_hooks.h"
 static Bit8u custom_runs;
 
-//static class custom_prog running_progs[2];
-
-//static class custom_prog *current;
-
 void custom_init_prog(char *name, Bit16u relocate, Bit16u init_cs, Bit16u init_ip)
 {
 /*
@@ -22,8 +18,18 @@ void custom_init_prog(char *name, Bit16u relocate, Bit16u init_cs, Bit16u init_i
 */
 
 	/* run all detectors */
-	if (init_schick(name, relocate, init_cs, init_ip))
+#ifdef DOSBOX_SCHICK_H
+	if (schick_init(name, relocate, init_cs, init_ip))
 		custom_runs++;
+#endif /* DOSBOX_SCHICK_H */
+#ifdef DOSBOX_SCHWEIF_H
+	if (schweif_init(name, relocate, init_cs, init_ip))
+		custom_runs++;
+#endif /* DOSBOX_SCHWEIF_H */
+#ifdef DOSBOX_RIVA_H
+	if (riva_init(name, relocate, init_cs, init_ip))
+		custom_runs++;
+#endif /* DOSBOX_RIVA_H */
 }
 
 void custom_exit_prog(Bit8u exitcode)
@@ -32,15 +38,32 @@ void custom_exit_prog(Bit8u exitcode)
 		return;
 
 	custom_runs--;
-	exit_schick(exitcode);
+
+#ifdef DOSBOX_SCHICK_H
+	schick_exit(exitcode);
+#endif /* DOSBOX_SCHICK_H */
+#ifdef DOSBOX_SCHWEIF_H
+	schweif_exit(exitcode);
+#endif /* DOSBOX_SCHWEIF_H */
+#ifdef DOSBOX_RIVA_H
+	riva_exit(exitcode);
+#endif /* DOSBOX_RIVA_H */
 }
 
 int custom_calln(Bit16u ip)
 {
 	if (!custom_runs)
 		return 0;
-
+	
+#ifdef DOSBOX_SCHICK_H
 	return schick_calln16(ip);
+#endif /* DOSBOX_SCHICK_H */
+#ifdef DOSBOX_SCHWEIF_H
+	return schweif_calln(ip);
+#endif /* DOSBOX_SCHWEIF_H */
+#ifdef DOSBOX_RIVA_H
+	return riva_calln(ip);
+#endif /* DOSBOX_RIVA_H */
 }
 
 int custom_callf(Bitu cs, Bitu ip)
@@ -48,12 +71,19 @@ int custom_callf(Bitu cs, Bitu ip)
 	if (!custom_runs)
 		return 0;
 
+#ifdef DOSBOX_SCHICK_H
 	return schick_callf(cs, ip);
+#endif /* DOSBOX_SCHICK_H */
+#ifdef DOSBOX_SCHWEIF_H
+	return schweif_callf(cs, ip);
+#endif /* DOSBOX_SCHWEIF_H */
+#ifdef DOSBOX_RIVA_H
+	return riva_callf(cs, ip);
+#endif /* DOSBOX_RIVA_H */
 }
 
 void custom_init(Section *sec)
 {
-//	custom_prog *p_current = new custom_prog;
 }
 
 #endif /* DOSBOX_CUSTOM */
