d08e80f88637f8c9ddaf64ae56a0ba1cdd784894
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-03-30 14:58:08 +0200

    M302de: Bugfix: BE-BUG in GUI_lookup_char_width()

--- src/custom/schick/rewrite_m302de/seg096.cpp
+++ src/custom/schick/rewrite_m302de/seg096.cpp
@@ -407,22 +407,23 @@ unsigned short GUI_print_char(unsigned char c, unsigned short x, unsigned short 
 	return char_width;
 }
 //82b
-unsigned short GUI_lookup_char_width(unsigned char c, unsigned short *p){
+unsigned short GUI_lookup_char_width(unsigned char c, unsigned short *p)
+{
 	unsigned short i;
 
-	for (i = 0; i != 75*3; i += 3) {
+	for (i = 0; i != 75 * 3; i += 3) {
 		if (c != ds_readb(0xaa51 + i))
 			continue;
 
-		host_writew((Bit8u*)p, ds_readb(0xaa51 + i + 2) & 0xff);
+		*p = ds_readb(0xaa51 + i + 2) & 0xff;
 		return ds_readb(0xaa51 + i + 1) & 0xff;
 	}
 
 	if (c == 0x7e || c == 0xf0 || c == 0xf1 || c == 0xf2 || c == 0xf3) {
-		host_writew((Bit8u*)p, 0);
+		*p = 0;
 		return 0;
 	} else {
-		host_writew((Bit8u*)p, 5);
+		*p = 5;
 		return 0;
 	}
 }
