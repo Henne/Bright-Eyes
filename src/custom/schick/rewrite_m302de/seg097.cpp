dc785741a6cf6091a05fe3839edfd9195383aba6
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-03-30 15:19:12 +0200

    M302de: Bugfix: BE-BUG in GUI_lookup_char_height()

--- src/custom/schick/rewrite_m302de/seg097.cpp
+++ src/custom/schick/rewrite_m302de/seg097.cpp
@@ -27,23 +27,24 @@ namespace M302de {
 //000
 //129
 //15e
-char GUI_lookup_char_height(char c, unsigned short *p) {
+char GUI_lookup_char_height(char c, unsigned short *p)
+{
 	short i;
-	Bit8u *tab = MemBase + PhysMake(datseg, 0xab42);
+	Bit8u *tab = p_datseg + 0xab42;
 
 	for (i = 0; i != 210; i+=3) {
 		if (c != ds_readb(0xab42 + i))
 			continue;
 
-		host_writew((Bit8u*)p, ds_readb(0xab42 + i + 2) & 0xff);
+		*p = ds_readb(0xab42 + i + 2) & 0xff;
 		return ds_readb(0xab42 + i + 1) & 0xff;
 	}
 
 	if (c == 0x7e || c == 0xf0 || c == 0xf1 || c == 0xf2 || c == 0xf3) {
-		host_writew((Bit8u*)p, 0);
+		*p = 0;
 		return 0;
 	} else {
-		host_writew((Bit8u*)p, 8);
+		*p = 8;
 		return 0;
 	}
 }
