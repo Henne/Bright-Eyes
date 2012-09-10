cd6df07a74a71c280de22a02c426c70ddea27227
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-09-10 17:27:43 +0200

    M302de: sync signature of test_foe_range_attack()

--- src/custom/schick/rewrite_m302de/seg037.h
+++ src/custom/schick/rewrite_m302de/seg037.h
@@ -6,6 +6,6 @@ signed short copy_ani_stuff(Bit8u*, signed short, signed short);
 unsigned short test_foe_melee_attack(signed short, signed short, signed short, signed short, signed short);
 
 /* 0x2a */
-signed short test_foe_range_attack(signed short, signed short, signed short, signed short);
+signed short test_foe_range_attack(signed short, signed short, const signed short, signed short);
 
 }
