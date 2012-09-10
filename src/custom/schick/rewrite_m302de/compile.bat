153c12d3ef0367f086936b0998ba7457a6d9ebe7
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-09-10 16:59:40 +0200

    M302de: update compilation batchfile

--- src/custom/schick/rewrite_m302de/compile.bat
+++ src/custom/schick/rewrite_m302de/compile.bat
@@ -1,3 +1,4 @@
-REM ..\BIN\BCC.EXE -mlarge -Od -c SEG002.CPP
-..\BIN\BCC.EXE -mlarge -Od -c SEG036.CPP
-REM ..\BIN\BCC.EXE -mlarge -Od -c -Y SEG108.CPP
+@REM ..\BIN\BCC.EXE -mlarge -Od -c SEG002.CPP
+@REM..\BIN\BCC.EXE -mlarge -Od -c -Y SEG036.CPP
+..\BIN\BCC.EXE -mlarge -O- -c -k+ -1 -Y SEG037.CPP
+@REM ..\BIN\BCC.EXE -mlarge -Od -c -Y SEG108.CPP
