88f75ec801ee17dbdf82bd0feae13c954ed63298
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-09-12 12:58:34 +0200

    M302de: bc: borlandify seg101.h

--- src/custom/schick/rewrite_m302de/seg101.h
+++ src/custom/schick/rewrite_m302de/seg101.h
@@ -1,56 +1,60 @@
+#if !defined(__BORLANDC__)
 namespace M302de {
+#endif
 
-//0x20
+/* 0x20 */
 void spell_adler();
-//0x25
+/* 0x25 */
 void spell_arcano();
-//*0x2a
+/* 0x2a */
 void spell_armatrutz();
-//0x2f
+/* 0x2f */
 void spell_inc_ch();
-//0x34
+/* 0x34 */
 void spell_feuerbann();
-//0x39
+/* 0x39 */
 void spell_inc_ff();
-//0x3e
+/* 0x3e */
 void spell_inc_ge();
-//0x43
+/* 0x43 */
 void spell_inc_in();
-//0x48
+/* 0x48 */
 void spell_inc_kk();
-//0x4d
+/* 0x4d */
 void spell_inc_kl();
-//0x52
+/* 0x52 */
 void spell_inc_mu();
-//0x57
+/* 0x57 */
 void spell_mutabili();
-//0x5c
+/* 0x5c */
 void spell_paral();
-//0x61
+/* 0x61 */
 void spell_salander();
-//0x66
+/* 0x66 */
 void spell_see();
-//0x6b
+/* 0x6b */
 void spell_visibili();
 
 /* Transmutation / Veraenderung */
-//0x70
+/* 0x70 */
 void spell_abvenenum();
-//0x75
+/* 0x75 */
 void spell_aeolitus();
-//0x7f
+/* 0x7f */
 void spell_claudibus();
-//0x84
+/* 0x84 */
 void spell_dunkelheit();
-//0x89
+/* 0x89 */
 void spell_erstarre();
-//0x8e
+/* 0x8e */
 void spell_flimflam();
-//0x93
+/* 0x93 */
 void spell_schmelze();
-//0x98
+/* 0x98 */
 void spell_silentium();
-//0x9d
+/* 0x9d */
 void spell_sturmgebr();
 
+#if !defined(__BORLANDC__)
 }
+#endif
