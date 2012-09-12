d4464f17364f088fe94043b4b95acebf2dd3dd60
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-09-12 08:30:18 +0200

    M302de: bc: borlandify seg105.h

--- src/custom/schick/rewrite_m302de/seg105.h
+++ src/custom/schick/rewrite_m302de/seg105.h
@@ -1,25 +1,29 @@
+#if !defined(__BORLANDC__)
 namespace M302de {
+#endif
 
-//20
+/* 0x20 */
 void unequip(Bit8u *hero, unsigned short item, unsigned short pos);
-//2f
+/* 0x2f */
 signed short give_hero_new_item(Bit8u *hero, signed short, signed short, signed short);
-//34
+/* 0x34 */
 unsigned short drop_item(Bit8u *, unsigned short, signed short);
-//3e
+/* 0x3e */
 unsigned short can_hero_use_item(Bit8u *, unsigned short);
-//43
+/* 0x43 */
 unsigned short can_item_at_pos(unsigned short item, unsigned short pos);
-//48
+/* 0x48 */
 signed short has_hero_stacked(Bit8u *, unsigned short);
-//4d
-//static
+/* 0x4d */
+/* static */
 unsigned short item_pleasing_ingerimm(unsigned short);
-//57
+/* 0x57 */
 unsigned short hero_count_item(Bit8u *, unsigned short);
-//5c
+/* 0x5c */
 unsigned short group_count_item(unsigned short);
 /* 0x61 */
 void loose_random_item(Bit8u *, unsigned short, Bit8u*);
 
+#if !defined(__BORLANDC__)
 }
+#endif
