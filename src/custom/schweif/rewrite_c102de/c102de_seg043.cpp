80436254bb6231259566fba3abe53a9a7670527d
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-10-01 08:31:51 +0200

    c102de: bypass set_video_mode()

--- src/custom/schweif/rewrite_c102de/c102de_seg043.cpp
+++ src/custom/schweif/rewrite_c102de/c102de_seg043.cpp
@@ -1,13 +1,19 @@
 /*
  *	Rewrite of DSA2 c1.02_de functions seg043 (gfx_mode/palette)
- *	Functions rewritten 0/9
+ *	Functions rewritten 1/9
  *	Function unused 0/2
  */
 
 #include "paging.h"
+#include "../../ints/int10.h"
 
 #include "schweif.h"
 
 namespace C102de {
+
+void set_video_mode(unsigned char mode)
+{
+	INT10_SetVideoMode(mode);
+}
 
 }
