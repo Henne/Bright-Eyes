3696faf6c9c4271af2b4fa5bdfa270d3d5fc6996
Henrik Kretzschmar <henne@nachtwindheim.de>
2012-10-01 08:41:30 +0200

    c102de: bypass set_display_page()

--- src/custom/schweif/rewrite_c102de/c102de_seg043.cpp
+++ src/custom/schweif/rewrite_c102de/c102de_seg043.cpp
@@ -1,6 +1,6 @@
 /*
  *	Rewrite of DSA2 c1.02_de functions seg043 (gfx_mode/palette)
- *	Functions rewritten 1/9
+ *	Functions rewritten 2/9
  *	Function unused 0/2
  */
 
@@ -14,6 +14,11 @@ namespace C102de {
 void set_video_mode(unsigned char mode)
 {
 	INT10_SetVideoMode(mode);
+}
+
+void set_display_page(unsigned char page)
+{
+	INT10_SetActivePage(0x05 | page);
 }
 
 }
--- src/custom/schweif/rewrite_c102de/c102de_seg043.h
+++ src/custom/schweif/rewrite_c102de/c102de_seg043.h
@@ -2,4 +2,6 @@ namespace C102de {
 
 void set_video_mode(unsigned char);
 
+void set_display_page(unsigned char);
+
 }
