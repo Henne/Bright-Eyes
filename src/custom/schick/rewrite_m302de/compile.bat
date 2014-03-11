3cd0b141165cb585024458d0c36ee19a1d5caa57
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-03-11 14:46:27 +0100

    M302de: BC: sort DOS compile file

--- src/custom/schick/rewrite_m302de/compile.bat
+++ src/custom/schick/rewrite_m302de/compile.bat
@@ -4,27 +4,27 @@
 @REM -O2 Fastest Code -O -Ob -Oe -Og -Oi -Ol -Om -Op -Ot -Ov -k- -Z
 @REM
 @REM
-@REM ..\BIN\BCC.EXE -mlarge -Od -c SEG002.CPP
-..\BIN\BCC.EXE -mlarge -O- -c -1 -Y SEG001.CPP
+@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Y SEG001.CPP
+@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Y -j5 SEG002.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG024.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG027.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG029.CPP
-@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG072.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG036.CPP
-@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG037.CPP
+@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo -j5 SEG037.CPP
+@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG040.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo -j5 SEG044.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG068.CPP
+@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG072.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG075.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG095.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo -j5 SEG100.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo -j5 SEG101.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG108.CPP
+@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG120.CPP
 @REM
 @REM -- ready some work todo
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Y SEG006.CPP
 @REM
-@REM --here start the good ones --
-@REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG120.CPP
 @REM -- here start the complete
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Y SEG007.CPP
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG047.CPP
