6f20112d8a46786e3a017c5e2dbb7884ad8e2c18
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-05-10 23:02:29 +0200

    M302de: catch seg002 call

--- src/custom/schick/schick_m302de.cpp
+++ src/custom/schick/schick_m302de.cpp
@@ -1633,6 +1633,9 @@ static int seg002(unsigned short offs) {
 		D1_LOG("count_heros_available() = %d\n", reg_ax);
 		return 1;
 	}
+	case 0x57f1: {
+		return 0;
+	}
 	case 0x5799: {
 		reg_ax = count_heroes_available_in_group();
 		D1_LOG("count_heroes_available_in_group() = %d\n", reg_ax);
