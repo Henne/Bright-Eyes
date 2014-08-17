57dcd01fc8e2e45cee17f06b6ba110bd7c884029
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-08-17 16:14:05 +0200

    M302de: add missing seg074 files

---
+++ src/custom/schick/rewrite_m302de/seg074.cpp
@@ -0,0 +1,20 @@
+/**
+ *	Rewrite of DSA1 v3.02_de functions of seg074 ()
+ *	Functions rewritten: 0/11
+ */
+
+#include "v302de.h"
+
+#if !defined(__BORLANDC__)
+namespace M302de {
+#endif
+
+/* TODO: stub function for transversalis */
+signed short select_teleport_dest(void)
+{
+	return -1;
+}
+
+#if !defined(__BORLANDC__)
+}
+#endif
---
+++ src/custom/schick/rewrite_m302de/seg074.h
@@ -0,0 +1,10 @@
+#if !defined(__BORLANDC__)
+namespace M302de {
+#endif
+
+/* 0x25 */
+signed short select_teleport_dest(void);
+
+#if !defined(__BORLANDC__)
+}
+#endif
