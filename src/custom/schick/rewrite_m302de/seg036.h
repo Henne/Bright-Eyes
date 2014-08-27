d8232c78cc097731d11335a3785833b3a6cfe102
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-08-27 19:26:33 +0200

    M302de: BC: mark functions in seg036 which could be static

--- src/custom/schick/rewrite_m302de/seg036.h
+++ src/custom/schick/rewrite_m302de/seg036.h
@@ -2,31 +2,38 @@
 namespace M302de {
 #endif
 
-/* static */
+/* is static */
 signed short KI_copy_ani_sequence(Bit8u*, signed short, signed short);
 
 /* 0x20 */
 void seg036_00ae(Bit8u*, signed short);
 
 /* 0x25 */
+/* should be static */
 signed short KI_change_hero_weapon(Bit8u*);
 
 /* 0x2a */
+/* should be static */
 signed short KI_can_attack_neighbour(signed short, signed short, signed short,
 					signed short, signed short);
 /* 0x2f */
+/* should be static */
 signed short KI_search_spell_target(signed short, signed short, signed short, signed short);
 
 /* 0x34 */
+/* should be static */
 signed short KI_select_spell_target(Bit8u*, signed short, signed short, signed short, signed short);
 
 /* 0x39 */
+/* should be static */
 signed short KI_get_spell(signed short, signed short);
 
 /* 0x34 */
+/* should be static */
 signed short seg036_8cf(Bit8u*, signed short, signed short, signed short, signed short);
 
 /* 0x43 */
+/* should be static */
 signed short KI_count_heros(signed short);
 
 #if !defined(__BORLANDC__)
