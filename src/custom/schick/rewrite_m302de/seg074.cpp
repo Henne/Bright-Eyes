a67e69765ddd1036343e1e043f1bcd9344570071
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-09-01 11:43:11 +0200

    M302de: add code for show_automap()

--- src/custom/schick/rewrite_m302de/seg074.cpp
+++ src/custom/schick/rewrite_m302de/seg074.cpp
@@ -1,13 +1,147 @@
 /**
- *	Rewrite of DSA1 v3.02_de functions of seg074 ()
- *	Functions rewritten: 0/11
+ *	Rewrite of DSA1 v3.02_de functions of seg074 (automap)
+ *	Functions rewritten: 1/11
  */
 
 #include "v302de.h"
 
+#include "seg002.h"
+#include "seg004.h"
+#include "seg029.h"
+#include "seg074.h"
+#include "seg097.h"
+#include "seg103.h"
+
 #if !defined(__BORLANDC__)
 namespace M302de {
 #endif
+
+#if defined(__BORLANDC__)
+/* Borlandified and identical */
+void show_automap(void)
+{
+	signed short l_si;
+	signed short l_di;
+	signed short loc_bak;
+	signed short done;
+	signed short dungeon;
+	signed short town;
+	signed short bak;
+
+	if (!ds_readb(DUNGEON_LIGHT) ||
+		((ds_readbs(DUNGEON_LIGHT) == 1) && (test_skill(get_first_hero_available_in_group(), 28, 6) > 0)))
+	{
+
+		ds_writeb(0x45b8, 1);
+
+		dungeon = ds_readbs(DUNGEON_INDEX);
+		town = ds_readbs(CURRENT_TOWN);
+
+		ds_writeb(CURRENT_TOWN, ds_writeb(DUNGEON_INDEX, 0));
+
+		bak = ds_readw(0xbffd);
+		ds_writew(0xbffd, 3);
+
+		l_si = (ds_readb(0xbd94) == 16) ? 0 :
+				((ds_readws(X_TARGET) - 8 < 0) ? 0 :
+				((ds_readws(X_TARGET) - 8 > 15) ? 16 : ds_readws(X_TARGET) - 8));
+
+		ds_writeb(CURRENT_TOWN, (signed char)town);
+		ds_writeb(DUNGEON_INDEX, (signed char)dungeon);
+
+		ds_writew(0x2846, 1);
+
+		do {
+
+			if (ds_readw(0x2846) != 0) {
+				loc_bak = ds_readbs(LOCATION);
+				ds_writeb(LOCATION, 1);
+
+				draw_main_screen();
+				ds_writeb(LOCATION, (signed char)loc_bak);
+
+				if (ds_readb(0xbd94) == 16) {
+					draw_loc_icons(1, 8);
+				} else {
+					draw_loc_icons(3, 27, 26, 8);
+				}
+
+				seg074_305(l_si);
+				clear_ani_pal();
+				seg074_7b2();
+				set_ani_pal(p_datseg + 0x7d0e);
+				ds_writew(0x2846, 0);
+			}
+
+			done = 0;
+
+			handle_gui_input();
+
+			if ((ds_readw(0xc3d3) != 0) || (ds_readw(ACTION) == 73)) {
+
+				if (ds_readb(0xbd94) == 16) {
+					l_di = GUI_radio(get_ltx(0x990), 1, get_ltx(0x994)) - 1;
+				} else {
+					ds_writew(0xe5ac, 2);
+					l_di = GUI_radio(get_ltx(0x990), 3,
+								 get_ltx(0x998),
+								 get_ltx(0x99c),
+								 get_ltx(0x994)) - 1;
+				}
+
+				if (l_di != -2) {
+					ds_writew(ACTION, l_di + 129);
+				}
+			}
+
+			if (ds_readb(0xbd94) != 16) {
+
+				if ((ds_readws(ACTION) == 129) || (ds_readws(ACTION) == 75)) {
+
+					if (l_si > 0) {
+						seg074_305(--l_si);
+						seg074_7b2();
+					}
+				}
+
+				if ((ds_readws(ACTION) == 130) || (ds_readws(ACTION) == 77)) {
+
+					if (l_si < 16) {
+						seg074_305(++l_si);
+						seg074_7b2();
+					}
+				}
+			}
+
+			if (((ds_readws(ACTION) == 129) && (ds_readb(0xbd94) == 16)) ||
+				((ds_readws(ACTION) == 131) && (ds_readb(0xbd94) != 16)))
+			{
+				done = 1;
+			}
+		} while (done == 0);
+
+		ds_writew(0xbffd, bak);
+		ds_writew(0x2846, 1);
+
+		clear_ani_pal();
+
+	} else {
+		GUI_output(get_ltx(0xc10));
+	}
+}
+#endif
+
+#if defined(__BORLANDC__)
+void seg074_305(signed short a1)
+{
+}
+
+void seg074_7b2(void)
+{
+}
+
+#endif
+
 
 /* TODO: stub function for transversalis */
 signed short select_teleport_dest(void)
--- src/custom/schick/rewrite_m302de/seg074.h
+++ src/custom/schick/rewrite_m302de/seg074.h
@@ -2,8 +2,24 @@
 namespace M302de {
 #endif
 
+#if defined(__BORLANDC__)
+/* 0x20 */
+void show_automap(void);
+#endif
+
 /* 0x25 */
 signed short select_teleport_dest(void);
+
+#if defined(__BORLANDC__)
+/* 0x2f */
+/* should be static */
+void seg074_305(signed short);
+
+/* 0x3e */
+/* should be static */
+void seg074_7b2(void);
+#endif
+
 
 #if !defined(__BORLANDC__)
 }
