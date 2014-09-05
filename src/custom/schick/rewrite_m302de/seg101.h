04d25dee4e5592984465466d948911c2b2755f5e
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-09-05 12:02:10 +0200

    M302de: BC: beautify header of seg101

--- src/custom/schick/rewrite_m302de/seg101.h
+++ src/custom/schick/rewrite_m302de/seg101.h
@@ -3,59 +3,59 @@ namespace M302de {
 #endif
 
 /* 0x20 */
-void spell_adler();
+void spell_adler(void);
 /* 0x25 */
-void spell_arcano();
+void spell_arcano(void);
 /* 0x2a */
-void spell_armatrutz();
+void spell_armatrutz(void);
 /* 0x2f */
-void spell_inc_ch();
+void spell_inc_ch(void);
 /* 0x34 */
-void spell_feuerbann();
+void spell_feuerbann(void);
 /* 0x39 */
-void spell_inc_ff();
+void spell_inc_ff(void);
 /* 0x3e */
-void spell_inc_ge();
+void spell_inc_ge(void);
 /* 0x43 */
-void spell_inc_in();
+void spell_inc_in(void);
 /* 0x48 */
-void spell_inc_kk();
+void spell_inc_kk(void);
 /* 0x4d */
-void spell_inc_kl();
+void spell_inc_kl(void);
 /* 0x52 */
-void spell_inc_mu();
+void spell_inc_mu(void);
 /* 0x57 */
-void spell_mutabili();
+void spell_mutabili(void);
 /* 0x5c */
-void spell_paral();
+void spell_paral(void);
 /* 0x61 */
-void spell_salander();
+void spell_salander(void);
 /* 0x66 */
-void spell_see();
+void spell_see(void);
 /* 0x6b */
-void spell_visibili();
+void spell_visibili(void);
 
 /* Transmutation / Veraenderung */
 /* 0x70 */
-void spell_abvenenum();
+void spell_abvenenum(void);
 /* 0x75 */
-void spell_aeolitus();
+void spell_aeolitus(void);
 /* 0x7a */
-void spell_brenne();
+void spell_brenne(void);
 /* 0x7f */
-void spell_claudibus();
+void spell_claudibus(void);
 /* 0x84 */
-void spell_dunkelheit();
+void spell_dunkelheit(void);
 /* 0x89 */
-void spell_erstarre();
+void spell_erstarre(void);
 /* 0x8e */
-void spell_flimflam();
+void spell_flimflam(void);
 /* 0x93 */
-void spell_schmelze();
+void spell_schmelze(void);
 /* 0x98 */
-void spell_silentium();
+void spell_silentium(void);
 /* 0x9d */
-void spell_sturmgebr();
+void spell_sturmgebr(void);
 
 #if !defined(__BORLANDC__)
 }
