6ef94759ef93ccbc55fbdd82bfd59a1e93eb1335
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-09-24 07:45:27 +0200

    M302de: BC: borlandify add_hero_ap()

--- src/custom/schick/rewrite_m302de/seg002.cpp
+++ src/custom/schick/rewrite_m302de/seg002.cpp
@@ -4901,11 +4901,15 @@ void add_party_money(Bit32s money)
 }
 
 /**
-	add_hero_ap - add AP
+ * \brief	add AP to a hero
+ *
+ * \param hero	pointer to the hero
+ * \param ap	AP the hero should get
 */
+/* Borlandified and identical */
 void add_hero_ap(Bit8u *hero, Bit32s ap)
 {
-	host_writed(hero+0x28, host_readd(hero+0x28) + ap);
+	add_ptr_ds(hero + 0x28, ap);
 }
 
 /**
