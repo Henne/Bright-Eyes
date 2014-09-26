ea23edd6a076497b6e4f2e56a9e34e33f43ca40c
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-09-26 16:34:41 +0200

    M302de: BC: borlandify passages_init()

--- src/custom/schick/rewrite_m302de/seg063.cpp
+++ src/custom/schick/rewrite_m302de/seg063.cpp
@@ -11,44 +11,34 @@
 namespace M302de {
 #endif
 
+/* Borlandified and identical */
 void passages_init(void)
 {
-	Bit8u *p;
-	Bit16u si;
-	Bit16u i;
+	signed short si;
+	signed short i;
+	Bit8u *p = p_datseg + 0x6f00;
 
-	p = p_datseg + 0x6f00;
+
 	for (i = 0; i < 45; p += 8, i++) {
-		host_writeb(p + 4,
-			(unsigned char)random_interval(0, host_readb(p + 3)));
+
+		host_writeb(p + 4, (unsigned char)random_interval(0, host_readbs(p + 3)));
 		host_writeb(p + 7, (unsigned char)random_interval(70, 130));
 
 		si = random_schick(100);
-		if (host_readb(p + 5) == 0) {
+
+		if (!host_readbs(p + 5)) {
+
 			/* Hochseerouten */
-			if (si <= 50) {
-				host_writeb(p + 6, 0);
-			} else if (si <= 80) {
-				host_writeb(p + 6, 1);
-			} else if (si <= 95) {
-				host_writeb(p + 6, 2);
-			} else {
-				host_writeb(p + 6, 3);
-			}
+			host_writeb(p + 6, si <= 50 ? 0 : (si <= 80 ? 1 : (si <= 95 ? 2 : 3)));
+
 		} else {
-			if (si <= 10) {
-				host_writeb(p + 6, 4);
-			} else if (si <= 40) {
-				host_writeb(p + 6, 5);
-			} else if (si <= 80) {
-				host_writeb(p + 6, 6);
-			} else {
-				host_writeb(p + 6, 7);
-			}
+
+			/* Seerouten */
+			host_writeb(p + 6, si <= 10 ? 4 : (si <= 40 ? 5 : (si <= 80 ? 6 : 7)));
 		}
 
 #if !defined(__BORLANDC__)
-	D1_LOG("%s - %s: %d %d %d %d %d %d\n",
+	D1_LOG("%16s - %16s: %d %d %d %d %d %d\n",
 		(char*)get_ltx((host_readb(p + 0) + 0xeb) * 4),
 		(char*)get_ltx((host_readb(p + 1) + 0xeb) * 4),
 		host_readb(p + 2),
