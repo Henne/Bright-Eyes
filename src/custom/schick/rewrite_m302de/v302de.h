741266c6cac22f16962d12fb49713ee84975ee4a
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-11-05 14:23:39 +0100

    M302de: BC: add hero_dummy1() helper

--- src/custom/schick/rewrite_m302de/v302de.h
+++ src/custom/schick/rewrite_m302de/v302de.h
@@ -506,6 +506,15 @@ static inline unsigned short hero_unkn3(Bit8u *hero) {
 	else
 		return 1;
 }
+
+static inline unsigned short hero_dummy1(Bit8u *hero) {
+
+	if (((host_readb(hero + 0xab) >> 2) & 1) == 0)
+		return 0;
+	else
+		return 1;
+}
+
 /**
  * hero_transformed() -	check if hero is transformed
  * @hero:	ptr to hero
@@ -1067,6 +1076,7 @@ struct bittest {
 #define hero_unkn2(hero)	((*(struct hero_status*)(hero + 0xaa)).unkn2)
 
 #define hero_unkn3(hero)	((*(struct hero_status*)(hero + 0xaa)).unkn3)
+#define hero_dummy1(hero)	((*(struct hero_status*)(hero + 0xaa)).dummy1)
 #define hero_dup(hero)		((*(struct hero_status*)(hero + 0xaa)).dup)
 
 #define hero_transformed(hero)  ((*(struct hero_status*)(hero + 0xaa)).transf)
