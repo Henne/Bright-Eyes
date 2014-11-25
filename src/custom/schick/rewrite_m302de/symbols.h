6c585dcf37fee6fa2e73c71f621c249ca8efa5b6
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-11-25 12:15:26 +0100

    M302de: BC: add constant PTEMPLE_CLOSED

--- src/custom/schick/rewrite_m302de/symbols.h
+++ src/custom/schick/rewrite_m302de/symbols.h
@@ -52,6 +52,7 @@
 #define GREMOB_INVITED	(0x3f74)	/* unsigned char {0, 1} */
 #define HARLOT_DATE	(0x3f75)	/* unsigned char {0, 1} */
 #define DRUG_TIMER	(0x3f76)	/* unsigned char {0, 1, ..., 10} */
+#define PTEMPLE_CLOSED	(0x3f77)	/* unsigned char {0, 1} */
 #define ALRIK_DERONDAN	(0x3f78)	/* unsigned char {0, 1} */
 #define FOOD_MESSAGE	(0x4219)
 #define CITYINDEX	(0x4222)
