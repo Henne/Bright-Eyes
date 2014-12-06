cc253024beceaa395d98a5769d65027ee2fc6659
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2014-12-06 12:11:03 +0100

    M302de: TOOLS: add address to symbol converter

---
+++ src/custom/schick/rewrite_m302de/tools/add2sym.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# replaces addresses from the DS with symbolic names from symbols.h
+
+if [ ! -e symbols.h ]; then
+	echo "ERROR: symbols.h does not exist"
+	exit 1
+fi
+
+IFS=$'\n'
+for line in $(egrep "^#define " symbols.h | egrep "\(0x[0-9a-f]{4}\)"); do
+
+	# extract the name and the position
+	NAME=$(echo ${line} | cut -d " " -f 2 | cut -d $'\t' -f 1)
+	POS=$(echo ${line} | cut -d "(" -f 2 | cut -d ")" -f 1)
+
+#	echo "$line"
+	echo "replace $POS with $NAME"
+
+	for file in *.cpp; do
+
+		# look in the cpp-file for position
+		grep ${POS} $file
+
+		if [ $? -eq 0 ]; then
+			# found => replace position with name
+			sed -i -e "s/${POS}/${NAME}/" $file
+		fi
+	done
+done
