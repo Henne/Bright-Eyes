0948bd5edd27c61a563a230a0aef1dab46599e78
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2015-06-11 21:03:18 +0200

    M302de: bypass game_options()

--- src/custom/schick/schick_m302de.cpp
+++ src/custom/schick/schick_m302de.cpp
@@ -3018,12 +3018,8 @@ static int seg025(unsigned short offs) {
 	}
 	case 0x2a: {
 		D1_LOG("game_options();\n");
-#if 0
 		reg_ax = game_options();
 		return 1;
-#else
-		return 0;
-#endif
 	}
 	case 0x2f: {
 		D1_LOG("far copy_palette();\n");
