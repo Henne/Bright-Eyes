9ad462d8760a155069cbe684ebeb9e057fb7cdd0
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2015-06-29 18:20:54 +0200

    M302de: add code for city_event_switch()

--- src/custom/schick/rewrite_m302de/seg067.cpp
+++ src/custom/schick/rewrite_m302de/seg067.cpp
@@ -1,6 +1,6 @@
 /**
  *	Rewrite of DSA1 v3.02_de functions of seg067 (city)
- *	Functions rewritten: 0/13
+ *	Functions rewritten: 1/13
  */
 
 #include "v302de.h"
@@ -13,10 +13,42 @@
 namespace M302de {
 #endif
 
+/**
+ * \brief	select a random city event type
+ */
+/* Borlandified and identical */
 void city_event_switch(void)
 {
 #if !defined(__BORLANDC__)
 	DUMMY_WARNING();
+#else
+	/* load STRASSE.LTX */
+	load_buffer_1(224);
+
+	/* set city flag */
+	ds_writeb(C_EVENT_ACTIVE, 1);
+
+	switch (random_schick(9)) {
+		case 1: city_event_1(); break;
+		case 2: city_event_2(); break;
+		case 3: city_event_3(); break;
+		case 4: city_event_4(); break;
+		case 5: city_event_5(); break;
+		case 6: city_event_6(); break;
+		case 7: city_event_7(); break;
+		case 8: city_event_8(); break;
+		case 9: city_event_9(); break;
+	}
+
+	/* reset city flag */
+	ds_writeb(C_EVENT_ACTIVE, 0);
+
+	/* load the LTX-file of the current town */
+	load_buffer_1(ds_readbs(CURRENT_TOWN) + 77);
+
+	/* update the current position / make the step */
+	ds_writews(X_TARGET, ds_readws(0x2d83));
+	ds_writews(Y_TARGET, ds_readws(0x2d85));
 #endif
 }
 
