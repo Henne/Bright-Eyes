c7093173c8382208883164e5860d50e6656c9d0a
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2015-06-29 16:38:45 +0200

    M302de: add dummys functions in seg067.cpp

--- src/custom/schick/rewrite_m302de/seg067.cpp
+++ src/custom/schick/rewrite_m302de/seg067.cpp
@@ -5,9 +5,106 @@
 
 #include "v302de.h"
 
+#include "seg007.h"
+#include "seg026.h"
+#include "seg067.h"
+
 #if !defined(__BORLANDC__)
 namespace M302de {
 #endif
+
+void city_event_switch(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_1(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_2(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_3(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_4(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_5(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_6(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_7(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_8(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+void city_event_9(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+RealPt waffinfo_weapons(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+RealPt waffinfo_herbs(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+RealPt waffinfo_general(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#endif
+}
+
+
 #if !defined(__BORLANDC__)
 }
 #endif
--- src/custom/schick/rewrite_m302de/seg067.h
+++ src/custom/schick/rewrite_m302de/seg067.h
@@ -1,6 +1,34 @@
 #if !defined(__BORLANDC__)
 namespace M302de {
 #endif
+
+/* 0x20 */
+void city_event_1(void);
+/* 0x25 */
+void city_event_2(void);
+/* 0x2a */
+void city_event_5(void);
+/* 0x2f */
+void city_event_3(void);
+/* 0x34 */
+void city_event_4(void);
+/* 0x39 */
+void city_event_7(void);
+/* 0x3e */
+void city_event_8(void);
+/* 0x43 */
+void city_event_9(void);
+/* 0x48 */
+void city_event_6(void);
+/* 0x4d */
+void city_event_switch(void);
+/* 0x52 */
+RealPt waffinfo_weapons(void);
+/* 0x57 */
+RealPt waffinfo_herbs(void);
+/* 0x5c */
+RealPt waffinfo_general(void);
+
 #if !defined(__BORLANDC__)
 }
 #endif
