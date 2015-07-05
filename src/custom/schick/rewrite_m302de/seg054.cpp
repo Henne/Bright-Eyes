c4ea60963f814adeac610123e449e499d259308b
Henrik Kretzschmar <henrik.kretzschmar@s2011.tu-chemnitz.de>
2015-07-05 10:47:21 +0200

    M302de: add code for do_talk_inn()

--- src/custom/schick/rewrite_m302de/seg054.cpp
+++ src/custom/schick/rewrite_m302de/seg054.cpp
@@ -1,13 +1,25 @@
 /**
  *	Rewrite of DSA1 v3.02_de functions of seg054 (inn)
- *	Functions rewritten: 0/4
+ *	Functions rewritten: 1/4
  */
 
 #include "v302de.h"
 
+#include "seg031.h"
+
 #if !defined(__BORLANDC__)
 namespace M302de {
 #endif
+
+/* Borlandified and identical */
+void talk_inn(void)
+{
+#if !defined(__BORLANDC__)
+	DUMMY_WARNING();
+#else
+	do_random_talk(1, 0);
+#endif
+}
 
 void TLK_herberg(signed short state)
 {
--- src/custom/schick/rewrite_m302de/seg054.h
+++ src/custom/schick/rewrite_m302de/seg054.h
@@ -2,6 +2,9 @@
 namespace M302de {
 #endif
 
+/* 0x25 */
+void do_talk_inn(void);
+
 /* 0x2f */
 void TLK_herberg(signed short);
 
