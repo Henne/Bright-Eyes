10fc8a2e43cc4429194d67ea355f888be8ce87a0
Henrik Kretzschmar <henne@nachtwindheim.de>
2016-12-24 15:44:54 +0100

    M302de: use only one function for BC and non-BC-Code

--- src/custom/schick/rewrite_m302de/seg001.cpp
+++ src/custom/schick/rewrite_m302de/seg001.cpp
@@ -24,22 +24,47 @@
 #include "v302de.h"
 
 #include "seg000.h"
+#include "seg001.h"
 
 #define CDA_DATASEG (0x1238)
 
-#if !defined(__BORLANDC__)
-
+#if defined(__BORLANDC__)
+#include "seg013.h"
+#else
 #include "dosbox.h"
 #include "regs.h"
 #include "callback.h"
 #include "cpu.h"
+#endif
 
-#include "seg001.h"
 
+#if !defined(__BORLANDC__)
 namespace M302de {
+#endif
 
-static unsigned short CD_has_drives() {
+/**
+ * \brief	check if cdrom-drives are installed
+ *
+ * Returns: 0 - no drive, 1 - at least one drive
+ */
+/* Borlandified and identical */
+static unsigned short CD_has_drives(void)
+{
+#if defined(__BORLANDC__)
+	/* al ==  0: return number of drive letters */
+	asm {
+		mov ax, 0x1500
+		xor bx, bx
+		int 0x2f
+		xor ax, ax
+		or  bx, bx
+		jz has_cd
+		inc ax
+	}
+has_cd:
 
+	return _AX;
+#else
 	reg_ax = 0x1500;
 	reg_bx = 0x0000;
 	CALLBACK_RunRealInt(0x2f);
@@ -48,27 +73,54 @@ static unsigned short CD_has_drives() {
 		return 0;
 
 	return 1;
+#endif
 }
 
-static unsigned short CD_count_drives() {
+/* Borlandified and identical */
+static unsigned short CD_count_drives(void)
+{
+#if defined(__BORLANDC__)
+	asm {
+		mov ax, 0x1500
+		xor bx, bx
+		int 0x2f
+	}
+
+	return _BX;
+#else
 
 	reg_ax = 0x1500;
 	reg_bx = 0x0000;
 	CALLBACK_RunRealInt(0x2f);
 
 	return reg_bx;
+#endif
 }
 
-static unsigned short CD_get_first_drive() {
+/* Borlandified and identical */
+static unsigned short CD_get_first_drive(void)
+{
+#if defined(__BORLANDC__)
+	asm {
+		mov ax, 0x1500
+		xor bx, bx
+		int 0x2f
+	}
+
+	return _CX;
+#else
 
 	reg_ax = 0x1500;
 	reg_bx = 0x0000;
 	CALLBACK_RunRealInt(0x2f);
 
 	return reg_cx;
+#endif
 }
 
-unsigned short CD_set_drive_nr() {
+/* Borlandified and identical */
+unsigned short CD_set_drive_nr(void)
+{
 
 	if (CD_has_drives() == 0)
 		return 0;
@@ -80,24 +132,76 @@ unsigned short CD_set_drive_nr() {
 	return 1;
 }
 
-void CD_driver_request(RealPt req) {
+/* Borlandified and identical */
+#if defined(__BORLANDC__)
+void CD_driver_request(driver_request *req)
+#else
+void CD_driver_request(RealPt req)
+#endif
+{
+#if defined(__BORLANDC__)
+	asm {
+		mov ax, 0x1510
+		mov cx, [CD_DRIVE_NR]
+		les bx, req
+		int 0x2f
+	}
+#else
 	reg_ax = 0x1510;
 	reg_cx = ds_readw(CD_DRIVE_NR);
 	CPU_SetSegGeneral(es, RealSeg(req));
 	reg_bx = RealOff(req);
 	CALLBACK_RunRealInt(0x2f);
+#endif
 }
 
-unsigned int CD_get_tod() {
+/* Borlandified and identical */
+/* TODO: check adresses of seg013 */
+static void CD_unused1(void)
+{
+	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
+		return;
+#if defined(__BORLANDC__)
+	req[3].status = 0;
+	req[3].ptr = cd_buf1;
+	cd_buf1[0] = 0x0c;
+	CD_driver_request(&req[3]);
+#else
+	//DUMMY
+#endif
+}
+
+/**
+ * \brief	get time of day
+ *
+ * \return clock ticks since midnight, the system time.
+ *
+ * TODO: produces a compiler warning and is a bit hacky
+ */
+/* Borlandified and identical */
+unsigned int CD_get_tod(void)
+{
+#if defined(__BORLANDC__)
+	asm {
+		mov ah, 0x0
+		int 0x1a
+		mov ax, dx
+		mov dx, cx
+		jmp near leave_tod
+	}
+leave_tod:
+#else
 	reg_ah = 0;
 	CALLBACK_RunRealInt(0x1a);
 	reg_ax = reg_dx;
 	reg_dx = reg_cx;
 
 	return (reg_dx << 16) | reg_ax;
+#endif
 }
 
-void seg001_00c1(unsigned short track_nr) {
+void seg001_00c1(unsigned short track_nr)
+{
 	unsigned int track_start, track_end;
 	unsigned int track_len, tmp;
 
@@ -133,13 +237,17 @@ void seg001_00c1(unsigned short track_nr) {
 	track_len = track_end - track_start;
 	real_writed(reloc_game + CDA_DATASEG, 0x9e, track_len - 150);
 
+#if defined(__BORLANDC__)
 	CD_driver_request(RealMake(reloc_game + CDA_DATASEG, 0x8c));
+#else
+	CD_driver_request(RealMake(reloc_game + CDA_DATASEG, 0x8c));
+#endif
 	ds_writed(CD_AUDIO_POS, ((track_len - 150) * 0x1234e) / 0x4b000);
 	ds_writed(CD_AUDIO_TOD, CD_get_tod());
 }
 
-void seg001_02c4(void) {
-
+void seg001_02c4(void)
+{
 	signed int val;
 
 	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
@@ -160,34 +268,53 @@ void seg001_02c4(void) {
 	ds_writew(0x009b, 1);
 }
 
-signed short CD_bioskey(signed short cmd) {
+signed short CD_bioskey(signed short cmd)
+{
 	seg001_02c4();
 	return bc_bioskey(cmd);
 }
 
 /* CD_audio_stop_hsg() - stop audio playback in HSG format */
 /* static */
-void CD_audio_stop_hsg(void) {
+/* Borlandified and identical */
+void CD_audio_stop_hsg(void)
+{
 
 	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
 		return;
 
+#if defined(__BORLANDC__)
+	req[0].status = 0;
+	CD_driver_request(&req[0]);
+#else
 	real_writew(reloc_game + CDA_DATASEG, 3, 0);
 	CD_driver_request(RealMake(reloc_game + CDA_DATASEG, 0));
+#endif
+
 	ds_writew(0x009b, 0);
 }
 
-/* CD_audio_stop() - stop audio playback in HSG and redbook format */
-void CD_audio_stop() {
+/**
+ *\brief	stop audio playback in HSG and redbook format
+ */
+/* Borlandified and identical */
+void CD_audio_stop(void)
+{
 	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
 		return;
 
 	CD_audio_stop_hsg();
+#if defined(__BORLANDC__)
+	req[1].status = 0;
+	CD_driver_request(&req[1]);
+#else
 	real_writew(reloc_game + CDA_DATASEG, 0x1f, 0);
 	CD_driver_request(RealMake(reloc_game + CDA_DATASEG, 0x1c));
+#endif
 }
 
-void CD_audio_pause() {
+void CD_audio_pause(void)
+{
 	/* Is CD initialized ? */
 	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
 		return;
@@ -204,11 +331,16 @@ void CD_audio_pause() {
 	/* set current position to maximum singned int */
 	ds_writed(CD_AUDIO_POS, 0x7fffffff);
 
+#if defined(__BORLANDC__)
+	// DUMMY
+#else
 	real_writew(reloc_game + CDA_DATASEG, 0xab, 0);
 	CD_driver_request(RealMake(reloc_game + CDA_DATASEG, 0xa8));
+#endif
 }
 
-void CD_audio_play() {
+void CD_audio_play(void)
+{
 	/* Is CD initialized ? */
 	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
 		return;
@@ -223,16 +355,23 @@ void CD_audio_play() {
 	ds_writew(CD_AUDIO_PAUSED, 0);
 	ds_writed(CD_AUDIO_POS, ds_readd(CD_AUDIO_POS_BAK));
 	ds_writed(CD_AUDIO_TOD, CD_get_tod() - ds_readd(CD_AUDIO_TOD_BAK) + ds_readd(CD_AUDIO_TOD));
-
+#if defined(__BORLANDC__)
+	// DUMMY
+#else
 	real_writew(reloc_game + CDA_DATASEG, 0xc7, 0);
 	CD_driver_request(RealMake(reloc_game + CDA_DATASEG, 0xc4));
+#endif
 }
 
 void CD_set_track(signed short index)
 {
+#if defined(__BORLANDC__)
+	//DUMMY
+#else
 	CPU_Push16(index);
 	CALLBACK_RunRealFar(reloc_game + 0x4ac, 0x4f4);
 	CPU_Pop16();
+#endif
 }
 
 void CD_check(void)
@@ -245,150 +384,6 @@ void CD_init(void)
 	DUMMY_WARNING();
 }
 
+#if !defined(__BORLANDC__)
 }
-
-#else	/* __BORLANDC__ */
-
-
-#include "seg013.h"
-
-/**
- * CD_has_drives() - check if cdrom-drives are installed
- *
- * Returns: 0 - no drive, 1 - at least one drive
- */
-/* Borlandified and identical */
-static int CD_has_drives(void)
-{
-	/* al ==  0: return number of drive letters */
-	asm {
-		mov ax, 0x1500
-		xor bx, bx
-		int 0x2f
-		xor ax, ax
-		or  bx, bx
-		jz has_cd
-		inc ax
-	}
-has_cd:
-
-	return _AX;
-}
-
-/* Borlandified and identical */
-static int CD_count_drives(void)
-{
-
-	asm {
-		mov ax, 0x1500
-		xor bx, bx
-		int 0x2f
-	}
-
-	return _BX;
-}
-
-/* Borlandified and identical */
-static int CD_get_first_drive(void)
-{
-	asm {
-		mov ax, 0x1500
-		xor bx, bx
-		int 0x2f
-	}
-
-	return _CX;
-}
-
-/* Borlandified and identical */
-static int CD_set_drive_nr(void)
-{
-	if (CD_has_drives() == 0)
-		return 0;
-
-	if (CD_count_drives() == 0)
-		return 0;
-
-	ds_writew(CD_DRIVE_NR, CD_get_first_drive());
-
-	return 1;
-}
-
-/* Borlandified and identical */
-static void CD_driver_request(void * request)
-{
-	asm {
-		mov ax, 0x1510
-		mov cx, [CD_DRIVE_NR]
-		les bx, request
-		int 0x2f
-	}
-}
-
-/* Borlandified and identical */
-/* TODO: check adresses of seg013 */
-static void CD_unused1(void)
-{
-	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
-		return;
-
-	req[3].status = 0;
-	req[3].ptr = cd_buf1;
-	cd_buf1[0] = 0x0c;
-	CD_driver_request(&req[3]);
-}
-
-/**
- * CD_get_tod() - get time of day
- *
- * Returns: clock ticks since midnight, the system time.
- *
- * TODO: produces a compiler warning and is a bit hacky
- */
-/* Borlandified and identical */
-unsigned long CD_get_tod(void)
-{
-	asm {
-		mov ah, 0x0
-		int 0x1a
-		mov ax, dx
-		mov dx, cx
-		jmp near leave_tod
-	}
-leave_tod:
-}
-
-
-/* Borlandified and identical */
-void CD_audio_stop_hsg(void)
-{
-	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
-		return;
-
-	req[0].status = 0;
-	CD_driver_request(&req[0]);
-
-	ds_writew(0x009b, 0);
-}
-
-/* Borlandified and identical */
-void CD_audio_stop(void)
-{
-	if (ds_readw(CD_INIT_SUCCESSFUL) == 0)
-		return;
-
-	CD_audio_stop_hsg();
-	req[1].status = 0;
-	CD_driver_request(&req[1]);
-}
-
-void CD_set_track(signed short index)
-{
-	/* DUMMY */
-}
-
-void CD_check(void)
-{
-	/* DUMMY */
-}
-#endif	/* __BORLANDC__ */
+#endif
--- src/custom/schick/rewrite_m302de/seg001.h
+++ src/custom/schick/rewrite_m302de/seg001.h
@@ -3,20 +3,20 @@ namespace M302de {
 #endif
 
 //static
-unsigned short CD_set_drive_nr();
+unsigned short CD_set_drive_nr(void);
 //static
 void CD_driver_request(RealPt req);
 //static
-unsigned int CD_get_tod();
+unsigned int CD_get_tod(void);
 //static
 void seg001_00c1(unsigned short);
 void seg001_02c4(void);
 signed short CD_bioskey(signed short);
 //static
 void CD_audio_stop_hsg(void);
-void CD_audio_stop();
-void CD_audio_pause();
-void CD_audio_play();
+void CD_audio_stop(void);
+void CD_audio_pause(void);
+void CD_audio_play(void);
 void CD_set_track(signed short);
 void CD_check(void);
 void CD_init(void);
