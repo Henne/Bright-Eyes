735bbf66822f2ee2f2c830a642a11c24fc39bf41
Henrik Kretzschmar <henne@nachtwindheim.de>
2017-01-23 14:59:04 +0100

    M302de: BC: document some BCC compiler options

--- src/custom/schick/rewrite_m302de/compile.bat
+++ src/custom/schick/rewrite_m302de/compile.bat
@@ -6,6 +6,16 @@
 @REM
 @REM --the hard cases
 @REM -Os, -Od, -k- geht auch
+@REM option -P can be enabled explicit	(force C++ compilation)
+@REM option -k can be enabled explicit	(standart stack frame), but -k- also works
+@REM option -a- can be enabled explicit	(disable word alignment)
+@REM option -mlarge or -ml! are allowed	(memory model large with or without DSSTACK)
+@REM option -K must be disabled 	(default char is unsigned), but this max be changed in the code
+@REM option -1 can be enabled explicit	(80186 instructions), -2 also works, but never disable it with -1-
+@REM option -Fm is not enabled		(shortcut for -Fc, -Ff and -Fs)
+@REM option -Fc can be enabled explicit	(generate communal variables)
+@REM option -Ff must be disabled	(generate far variables)
+@REM option -Fs must be disabled	(assume ss == ds), but only 4 files are affected
 @REM ..\BIN\BCC.EXE -mlarge -O- -c -1 -Yo SEG106.CPP
 @REM
 @REM --the compiling cases
