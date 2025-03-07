41bb9be433f204cfda1d72667e685701984cb96b
Henne <119480+Henne@users.noreply.github.com>
2025-03-07 20:04:13 +0100

    SOA: add __BORLANDC__ for seg009

--- src/custom/soa/rewrite_soa_vgame_en/soa_seg009.cpp
+++ src/custom/soa/rewrite_soa_vgame_en/soa_seg009.cpp
@@ -7,15 +7,19 @@
  *	Call:		BCC.EXE -mlarge -O- -c -1 -Y -IAIL seg002.cpp
  *
  */
+#if !defined(__BORLANDC__)
 #include "dosbox.h"
 #include "callback.h"
 #include "dos_inc.h"
 
 #include "soa.h"
+#endif
 
 #include "soa_seg000.h"
 
+#if !defined(__BORLANDC__)
 namespace SOA_VGAME_EN {
+#endif
 
 // Remark: written in inline asm with parameter
 unsigned short swap_word(unsigned short inval)
@@ -29,4 +33,6 @@ unsigned short swap_word_fast(unsigned short inval)
 	return (((inval & 0xff00) >> 8) | ((inval & 0xff) << 8));
 }
 
+#if !defined(__BORLANDC__)
 }
+#endif
--- src/custom/soa/rewrite_soa_vgame_en/soa_seg009.h
+++ src/custom/soa/rewrite_soa_vgame_en/soa_seg009.h
@@ -1,4 +1,10 @@
+#if !defined(__BORLANDC__)
 namespace SOA_VGAME_EN {
+#endif
+
 unsigned short swap_word(unsigned short inval);
 unsigned short swap_word_fast(unsigned short inval);
+
+#if !defined(__BORLANDC__)
 }
+#endif
