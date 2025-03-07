712a9ea2130cf9a5a267252329cd95f7b2f6652c
Henne <119480+Henne@users.noreply.github.com>
2025-03-07 15:27:35 +0100

    TOOLS: remove helpers.h

--- src/custom/schick/rewrite_m302de/tools/dump_obj.cpp
+++ src/custom/schick/rewrite_m302de/tools/dump_obj.cpp
@@ -4,10 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "helpers.h"
-
 using namespace std;
 
+/* accessors in Little Endian */
+static inline unsigned char read_u8(const unsigned char *b)
+{
+	return *(unsigned char*)(b);
+}
+
+static inline unsigned short read_u16(const unsigned char *b)
+{
+	return (unsigned short)(read_u8(b) + (read_u8(b + 1) << 8));
+}
+
+static inline unsigned int read_u32(const unsigned char *b)
+{
+	return (unsigned int)(read_u16(b) + (read_u16(b + 2) << 16));
+}
+
+
+/* dump_obj logic */
 bool checksum_correct(const unsigned char *b, size_t len)
 {
 	unsigned char sum  = 0;
