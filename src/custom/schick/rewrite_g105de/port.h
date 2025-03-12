e46bc5d715ca4cc6a6998b863aaefe954abf2367
Henne <119480+Henne@users.noreply.github.com>
2025-03-12 08:44:18 +0100

    G105de: update port.h with signed host_ macros

--- src/custom/schick/rewrite_g105de/port.h
+++ src/custom/schick/rewrite_g105de/port.h
@@ -40,9 +40,17 @@ extern char ds[0xffff];
 #define host_readw(p) *(Bit16u*)(p)
 #define host_readd(p) *(Bit32u*)(p)
 
+#define host_readbs(p) *(Bit8s*)(p)
+#define host_readws(p) *(Bit16s*)(p)
+#define host_readds(p) *(Bit32s*)(p)
+
 #define host_writeb(p, d)       (*(Bit8u*)(p) = d)
 #define host_writew(p, d)       (*(Bit16u*)(p) = d)
 #define host_writed(p, d)       (*(Bit32u*)(p) = d)
+
+#define host_writebs(p, d)       (*(Bit8s*)(p) = d)
+#define host_writews(p, d)       (*(Bit16s*)(p) = d)
+#define host_writeds(p, d)       (*(Bit32s*)(p) = d)
 
 #define RealMake(seg, off) (void*)(seg << 4 + off)
 #define Real2Host(p) (p)
