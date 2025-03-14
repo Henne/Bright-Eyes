f0bd756c31d395d66559fb31c752a4c7d5007465
Henne <119480+Henne@users.noreply.github.com>
2025-03-14 11:00:39 +0100

    G105de: remove tabs from symbols.h

--- src/custom/schick/rewrite_g105de/symbols.h
+++ src/custom/schick/rewrite_g105de/symbols.h
@@ -4,6 +4,8 @@
  *	Here are the mappings from adresses in the datasegment
  *	to symbolic names of global variables.
  *	This will only work with the version v302de.
+ *
+ *	NOTE: between symbol names and adresses are only spaces allowed! NO TABS!
  */
 
 #if !defined SYMBOLS_H
@@ -33,85 +35,85 @@
 
 #endif
 
-#define MOUSE_MASK			(0x1200)
-#define MOUSE_POSY_MIN			(0x1240)
-#define MOUSE_POSX_MIN			(0x1242)
-#define MOUSE_POSY_MAX			(0x1244)
-#define MOUSE_POSX_MAX			(0x1246)
-#define MOUSE_LOCKED			(0x1248)
-#define MOUSE_REFRESH_FLAG		(0x124a)    /* signed short */
-#define MOUSE_POSX			(0x124c)
-#define MOUSE_POSY			(0x124e)
+#define MOUSE_MASK                      (0x1200)
+#define MOUSE_POSY_MIN                  (0x1240)
+#define MOUSE_POSX_MIN                  (0x1242)
+#define MOUSE_POSY_MAX                  (0x1244)
+#define MOUSE_POSX_MAX                  (0x1246)
+#define MOUSE_LOCKED                    (0x1248)
+#define MOUSE_REFRESH_FLAG              (0x124a)    /* signed short */
+#define MOUSE_POSX                      (0x124c)
+#define MOUSE_POSY                      (0x124e)
 
-#define MOUSE_MOVED			(0x1254)
+#define MOUSE_MOVED                     (0x1254)
 
-#define DEFAULT_ACTION			(0x1272)    /* RealPt */
-#define ACTION_TABLE			(0x1276)    /* RealPt */
+#define DEFAULT_ACTION                  (0x1272)    /* RealPt */
+#define ACTION_TABLE                    (0x1276)    /* RealPt */
 
-#define GEN_PAGE			(0x1324)    /* signed short */
+#define GEN_PAGE                        (0x1324)    /* signed short */
 
-#define MIDI_DISABLED			(0x1a07)    /* signed short */
-#define USE_CDA				(0x1a09)    /* signed short */
-#define MOUSE_HANDLER_INSTALLED		(0x1a0b)    /* signed short */
+#define MIDI_DISABLED                   (0x1a07)    /* signed short */
+#define USE_CDA	                        (0x1a09)    /* signed short */
+#define MOUSE_HANDLER_INSTALLED         (0x1a0b)    /* signed short */
 
-#define ACTION_INPUT			(0x1c63)
-#define ACTION_PAGE			(0x1c79)    /* RealPt[11] */
+#define ACTION_INPUT                    (0x1c63)
+#define ACTION_PAGE                     (0x1c79)    /* RealPt[11] */
 
-#define STR_SOUND_CFG			(0x1dd0)    /* char[9]; "SOUND.CFG" */
-#define STR_SOUNDHW_NOT_FOUND		(0x1de4)    /* char[]; "SOUND HARDWARE NOT FOUND" */
+#define STR_SOUND_CFG                   (0x1dd0)    /* char[9]; "SOUND.CFG" */
+#define STR_SOUNDHW_NOT_FOUND           (0x1de4)    /* char[]; "SOUND HARDWARE NOT FOUND" */
 
-#define RANDOM_GEN_SEED			(0x1fd6)    /* Bit16u */
+#define RANDOM_GEN_SEED                 (0x1fd6)    /* Bit16u */
 
 #define CD_AUDIO_TRACK                  (0x245a)    /* unsigned short */
 #define CD_AUDIO_TOD                    (0x2464)    /* unsigned long */
 #define CD_AUDIO_POS                    (0x2468)    /* unsigned long */
 #define CD_DRIVE_NO                     (0x246c)    /* unsigned short */
 
-#define CURRENT_TIMBRE_LENGTH		(0x2474)    /* Bit16u */
-#define CURRENT_TIMBRE_PATCH		(0x2476)    /* Bit8s */
-#define CURRENT_TIMBRE_BANK		(0x2477)    /* Bit8s */
-#define CURRENT_TIMBRE_OFFSET		(0x2478)    /* Bit32u */
+#define CURRENT_TIMBRE_LENGTH           (0x2474)    /* Bit16u */
+#define CURRENT_TIMBRE_PATCH            (0x2476)    /* Bit8s */
+#define CURRENT_TIMBRE_BANK             (0x2477)    /* Bit8s */
+#define CURRENT_TIMBRE_OFFSET           (0x2478)    /* Bit32u */
 
 
-#define FLEN				(0x3f2a)    /* Bit32s */
-#define FLEN_LEFT			(0x3f2e)    /* Bit32s */
-#define IRQ78_BAK			(0x3f32)    /* RealPt */
-#define GENDAT_OFFSET			(0x3f36)    /* Bit32s */
-#define HANDLE_TIMBRE			(0x3f3a)    /* Bit16s */
-#define TIMBRE_CACHE_SIZE		(0x3f3c)    /* Bit16s */
-#define STATE_TABLE_SIZE		(0x3f3e)    /* Bit32u */
-#define SND_DRIVER			(0x3f42)    /* RealPt */
-#define FORM_XMID			(0x3f46)    /* RealPt */
-#define SND_PTR_UNKN1			(0x3f4a)    /* RealPt */
-#define STATE_TABLE			(0x3f4e)    /* RealPt */
+#define FLEN                            (0x3f2a)    /* Bit32s */
+#define FLEN_LEFT                       (0x3f2e)    /* Bit32s */
+#define IRQ78_BAK                       (0x3f32)    /* RealPt */
+#define GENDAT_OFFSET                   (0x3f36)    /* Bit32s */
+#define HANDLE_TIMBRE                   (0x3f3a)    /* Bit16s */
+#define TIMBRE_CACHE_SIZE               (0x3f3c)    /* Bit16s */
+#define STATE_TABLE_SIZE                (0x3f3e)    /* Bit32u */
+#define SND_DRIVER                      (0x3f42)    /* RealPt */
+#define FORM_XMID                       (0x3f46)    /* RealPt */
+#define SND_PTR_UNKN1                   (0x3f4a)    /* RealPt */
+#define STATE_TABLE                     (0x3f4e)    /* RealPt */
 
-#define SND_SEQUENCE			(0x3f5a)    /* Bit16s */
-#define SND_DRIVER_HANDLE		(0x3f5c)    /* Bit16s */
+#define SND_SEQUENCE                    (0x3f5a)    /* Bit16s */
+#define SND_DRIVER_HANDLE               (0x3f5c)    /* Bit16s */
 
-#define MENU_TILES			(0x40b9)    /* Bit16s */
+#define MENU_TILES                      (0x40b9)    /* Bit16s */
 
-#define TEXTS				(0x40d9)    /* RealPt array */
+#define TEXTS                           (0x40d9)    /* RealPt array */
 
-#define RANDOM_GEN_SEED2		(0x458f)    /* Bit16u */
+#define RANDOM_GEN_SEED2                (0x458f)    /* Bit16u */
 
-#define WO_VAR				(0x4595)    /* Bit16u */
+#define WO_VAR                          (0x4595)    /* Bit16u */
 
-#define IN_KEY_ASCII			(0x459d)    /* Bit16s */
-#define IN_KEY_EXT			(0x459f)    /* Bit16s */
+#define IN_KEY_ASCII                    (0x459d)    /* Bit16s */
+#define IN_KEY_EXT                      (0x459f)    /* Bit16s */
 
-#define HAVE_MOUSE			(0x4591)    /* Bit16s */
+#define HAVE_MOUSE                      (0x4591)    /* Bit16s */
 
-#define MOUSE1_EVENT1			(0x4597)    /* Bit16s */
-#define MOUSE2_EVENT			(0x4599)    /* Bit16s */
-#define MOUSE1_EVENT2			(0x459b)    /* Bit16s */
+#define MOUSE1_EVENT1                   (0x4597)    /* Bit16s */
+#define MOUSE2_EVENT                    (0x4599)    /* Bit16s */
+#define MOUSE1_EVENT2                   (0x459b)    /* Bit16s */
 
-#define MOUSE_LAST_CURSOR		(0x4621)
-#define MOUSE_CURRENT_CURSOR		(0x4625)
+#define MOUSE_LAST_CURSOR               (0x4621)
+#define MOUSE_CURRENT_CURSOR            (0x4625)
 
-#define MOUSE_BACKBUFFER		(0x4669)
+#define MOUSE_BACKBUFFER                (0x4669)
 
-#define BUFFER_TEXT			(0x4775)
-#define BUFFER_FONT6			(0x4779)
+#define BUFFER_TEXT                     (0x4775)
+#define BUFFER_FONT6                    (0x4779)
 
-#define VGA_MEMSTART			(0x47cb)     /* RealPt */
+#define VGA_MEMSTART                    (0x47cb)     /* RealPt */
 #endif
