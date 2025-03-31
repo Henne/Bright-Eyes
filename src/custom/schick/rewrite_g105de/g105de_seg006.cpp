3c15b1980bead5ee94904997e6cb60f37c347dbf
Henne <119480+Henne@users.noreply.github.com>
2025-03-31 21:20:47 +0200

    G105de: rework and complete AIL callbacks

--- src/custom/schick/rewrite_g105de/g105de_seg006.cpp
+++ src/custom/schick/rewrite_g105de/g105de_seg006.cpp
@@ -20,7 +20,62 @@ void AIL_shutdown(RealPt signoff_msg)
 	CPU_Pop32();
 }
 
-Bit16u AIL_register_sequence(Bit16u driver, RealPt FORM_XMID, Bit16u sequence_num, RealPt state_table, RealPt controller_table)
+Bit16s AIL_register_driver(RealPt driver_base_addr)
+{
+	CPU_Push32(driver_base_addr);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xa24);
+	CPU_Pop32();
+	return reg_ax;
+}
+
+RealPt AIL_describe_driver(Bit16s driver)
+{
+	CPU_Push16(driver);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xadc);
+	CPU_Pop16();
+	return RealMake(reg_dx, reg_ax);
+}
+
+Bit16u AIL_detect_device(Bit16s driver, Bit16u IO_addr, Bit16u IRQ, Bit16u DMA, Bit16u DRQ)
+{
+	CPU_Push16(DRQ);
+	CPU_Push16(DMA);
+	CPU_Push16(IRQ);
+	CPU_Push16(IO_addr);
+	CPU_Push16(driver);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xb02);
+	CPU_Pop16();
+	CPU_Pop16();
+	CPU_Pop16();
+	CPU_Pop16();
+	CPU_Pop16();
+	return reg_ax;
+}
+
+void AIL_init_driver(Bit16s driver, Bit16u IO_addr, Bit16u IRQ, Bit16u DMA, Bit16u DRQ)
+{
+	CPU_Push16(DRQ);
+	CPU_Push16(DMA);
+	CPU_Push16(IRQ);
+	CPU_Push16(IO_addr);
+	CPU_Push16(driver);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xb08);
+	CPU_Pop16();
+	CPU_Pop16();
+	CPU_Pop16();
+	CPU_Pop16();
+	CPU_Pop16();
+}
+
+Bit16u AIL_state_table_size(Bit16s driver)
+{
+	CPU_Push16(driver);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xc47);
+	CPU_Pop16();
+	return reg_ax;
+}
+
+Bit16s AIL_register_sequence(Bit16s driver, RealPt FORM_XMID, Bit16u sequence_num, RealPt state_table, RealPt controller_table)
 {
 	CPU_Push32(controller_table);
 	CPU_Push32(state_table);
@@ -36,7 +91,7 @@ Bit16u AIL_register_sequence(Bit16u driver, RealPt FORM_XMID, Bit16u sequence_nu
 	return reg_ax;
 }
 
-void AIL_release_sequence_handle(Bit16u driver, Bit16u sequence)
+void AIL_release_sequence_handle(Bit16s driver, Bit16s sequence)
 {
 	CPU_Push16(sequence);
 	CPU_Push16(driver);
@@ -45,7 +100,26 @@ void AIL_release_sequence_handle(Bit16u driver, Bit16u sequence)
 	CPU_Pop16();
 }
 
-Bit16u AIL_timbre_request(Bit16u driver, Bit16u sequence)
+Bit16u AIL_default_timbre_cache_size(Bit16s driver)
+{
+	CPU_Push16(driver);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xc59);
+	CPU_Pop16();
+	return reg_ax;
+}
+
+void AIL_define_timbre_cache(Bit16s driver, RealPt cache_addr, Bit16u cache_size)
+{
+	CPU_Push16(cache_size);
+	CPU_Push32(cache_addr);
+	CPU_Push16(driver);
+	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xc5f);
+	CPU_Pop16();
+	CPU_Pop32();
+	CPU_Pop16();
+}
+
+Bit16u AIL_timbre_request(Bit16s driver, Bit16s sequence)
 {
 	CPU_Push16(sequence);
 	CPU_Push16(driver);
@@ -55,7 +129,7 @@ Bit16u AIL_timbre_request(Bit16u driver, Bit16u sequence)
 	return reg_ax;
 }
 
-void AIL_install_timbre(Bit16u driver, Bit16u bank, Bit16u patch, RealPt src_addr)
+void AIL_install_timbre(Bit16s driver, Bit16s bank, Bit16s patch, RealPt src_addr)
 {
 	CPU_Push32(src_addr);
 	CPU_Push16(patch);
@@ -68,7 +142,7 @@ void AIL_install_timbre(Bit16u driver, Bit16u bank, Bit16u patch, RealPt src_add
 	CPU_Pop32();
 }
 
-void AIL_start_sequence(Bit16u driver, Bit16u sequence)
+void AIL_start_sequence(Bit16s driver, Bit16s sequence)
 {
 	CPU_Push16(sequence);
 	CPU_Push16(driver);
@@ -77,9 +151,8 @@ void AIL_start_sequence(Bit16u driver, Bit16u sequence)
 	CPU_Pop16();
 }
 
-void AIL_stop_sequence(Bit16u driver, Bit16u sequence)
+void AIL_stop_sequence(Bit16s driver, Bit16s sequence)
 {
-
 	CPU_Push16(sequence);
 	CPU_Push16(driver);
 	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xc89);
@@ -87,15 +160,13 @@ void AIL_stop_sequence(Bit16u driver, Bit16u sequence)
 	CPU_Pop16();
 }
 
-Bit16u AIL_sequence_status(Bit16u driver, Bit16u sequence)
+Bit16u AIL_sequence_status(Bit16s driver, Bit16s sequence)
 {
-
 	CPU_Push16(sequence);
 	CPU_Push16(driver);
 	CALLBACK_RunRealFar(reloc_gen + 0xbb2, 0xc95);
 	CPU_Pop16();
 	CPU_Pop16();
-
 	return reg_ax;
 }
 
--- src/custom/schick/rewrite_g105de/g105de_seg006.h
+++ src/custom/schick/rewrite_g105de/g105de_seg006.h
@@ -5,19 +5,33 @@ void AIL_startup();
 
 void AIL_shutdown(RealPt signoff_msg);
 
-Bit16u AIL_register_sequence(Bit16u driver, RealPt FORM_XMID, Bit16u sequence_num, RealPt state_table, RealPt controller_table);
+Bit16s AIL_register_driver(RealPt driver_base_addr);
 
-Bit16u AIL_timbre_request(Bit16u driver, Bit16u sequence);
+RealPt AIL_describe_driver(Bit16s driver);
 
-void AIL_install_timbre(Bit16u driver, Bit16u bank, Bit16u patch, RealPt src_addr);
+Bit16u AIL_detect_device(Bit16s driver, Bit16u IO_addr, Bit16u IRQ, Bit16u DMA, Bit16u DRQ);
 
-void AIL_start_sequence(Bit16u driver, Bit16u sequence);
+void AIL_init_driver(Bit16s driver, Bit16u IO_addr, Bit16u IRQ, Bit16u DMA, Bit16u DRQ);
 
-void AIL_stop_sequence(Bit16u driver, Bit16u sequence);
+Bit16u AIL_state_table_size(Bit16s driver);
 
-Bit16u AIL_sequence_status(Bit16u driver, Bit16u sequence);
+Bit16s AIL_register_sequence(Bit16s driver, RealPt FORM_XMID, Bit16u sequence_num, RealPt state_table, RealPt controller_table);
 
-void AIL_release_sequence_handle(Bit16u driver, Bit16u sequence);
+void AIL_release_sequence_handle(Bit16s driver, Bit16s sequence);
+
+Bit16u AIL_default_timbre_cache_size(Bit16s driver);
+
+void AIL_define_timbre_cache(Bit16s driver, RealPt cache_addr, Bit16u cache_size);
+
+Bit16u AIL_timbre_request(Bit16s driver, Bit16s sequence);
+
+void AIL_install_timbre(Bit16s driver, Bit16s bank, Bit16s patch, RealPt src_addr);
+
+void AIL_start_sequence(Bit16s driver, Bit16s sequence);
+
+void AIL_stop_sequence(Bit16s driver, Bit16s sequence);
+
+Bit16u AIL_sequence_status(Bit16s driver, Bit16s sequence);
 
 }
 #else
