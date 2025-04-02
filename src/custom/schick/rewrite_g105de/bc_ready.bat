ef94eb08b34fd7294dca426d515b4303e58b6c8b
Henne <119480+Henne@users.noreply.github.com>
2025-04-03 01:49:41 +0200

    G105de: add seg001 to bc_ready.bat

--- src/custom/schick/rewrite_g105de/bc_ready.bat
+++ src/custom/schick/rewrite_g105de/bc_ready.bat
@@ -3,8 +3,8 @@
 @REM They shall not change, so the are compared with every commit.
 @REM
 @REM -- here start the complete
-@REM BCC.EXE -mlarge -O- -c -1 -Y SEG001.CPP
-@REM BCC.EXE -mlarge -O- -c -1 -Y -IAIL SEG002.CPP
+BCC.EXE -mlarge -O- -c -2 SEG001.CPP
+BCC.EXE -mlarge -O -c -2 -IAIL SEG002.CPP
 BCC.EXE -mlarge -O2 -c -1 -Y SEG003.CPP
 TASM.EXE /os /z SEG004.asm SEG004.OBJ
 TASM.EXE /os /z SEG005.asm SEG005.OBJ
