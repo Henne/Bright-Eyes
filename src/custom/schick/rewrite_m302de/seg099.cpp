2d18b40afb8acf1efe1c08a72fb8033665e5e0f6
siebenstreich <siebenstreich@gmx.net>
2025-04-29 23:58:17 +0200

    resolved magic numbers and cleaner code around analues spell.

--- src/custom/schick/rewrite_m302de/seg099.cpp
+++ src/custom/schick/rewrite_m302de/seg099.cpp
@@ -1040,16 +1040,24 @@ RealPt spell_analues(void)
 
 	if (item_id) {
 
-		for (i = 0; ds_readws(ANALUES_ITEMS + i * 5) != -1; i++) {
+		for (i = 0; ds_readws(ANALUES_ITEMS + i * SIZEOF_ANALUES_ITEM + ANALUES_ITEM_ID) != -1; i++) {
 
-			if (ds_readws(ANALUES_ITEMS + i * 5) == item_id) {
+			if (ds_readws(ANALUES_ITEMS + i * SIZEOF_ANALUES_ITEM + ANALUES_ITEM_ID) == item_id) {
 
 				/* check if the spellcaster is able to analyze this item */
-				if (ds_readws((ANALUES_ITEMS + 2) + i * 5) <= ds_readws(SPELLTEST_RESULT)) {
+
+				/* The following is pretty hacky. The Analues spell was already performed, before calling this function,
+				 * without knowing the exact handicap. The handicap is considered only now, via comparing to the
+				 * SPELLTEST_RESULT global variable.
+				 * This a posteriori adjustment is only possible since not the original DSA 2/3 spell test logic is used,
+				 * but a lighter "pool version".
+				 * Also, it results in the Original-Bug that, in the case that the +0 spell test was successful, but with handicap it is not,
+				 * the full AE cost has to be payed. (Normally, you get a 1/2 reduction for a failed attempt.) */
+				if (ds_readws((ANALUES_ITEMS + ANALUES_ITEM_HANDICAP) + i * SIZEOF_ANALUES_ITEM) <= ds_readws(SPELLTEST_RESULT)) {
 
 					/* copy the matching result string */
 					strcpy((char*)Real2Host(ds_readd(TEXT_OUTPUT_BUF)),
-						(char*)get_tx(ds_readbs((ANALUES_ITEMS + 4) + i * 5)));
+						(char*)get_tx(ds_readbs((ANALUES_ITEMS + ANALUES_ITEM_DTP) + i * SIZEOF_ANALUES_ITEM)));
 
 					or_ptr_bs(get_spelluser() + item_pos * SIZEOF_INVENTORY + (HERO_INVENTORY + INVENTORY_FLAGS), 0x80); /* set 'magic_revealed' flag */
 					break;
